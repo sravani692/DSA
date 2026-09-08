311fe1f8a4a361a62fb6f7ec9c94a46288bc8d8a
sravani <sirasapallisravani45@gmail.com>
2026-09-08 13:59:20 +0530

    update

--- src/Demo/InterpolationSearch.java
+++ src/Demo/InterpolationSearch.java
@@ -1,5 +1,43 @@
+/*pos = low+(target-arr[low])*(high-low)/arr[high]-arr[low]
+ * pos means index value */
 package Demo;
 
 public class InterpolationSearch {
+	 static int interpolationSearch(int[] arr, int target) {
 
+	        int low = 0;
+	        int high = arr.length - 1;
+
+	        while (low <= high &&
+	               target >= arr[low] &&
+	               target <= arr[high]) {
+
+	            if (arr[low] == arr[high]) {
+	                if (arr[low] == target)
+	                    return low;
+	                return -1;
+	            }
+
+	            int pos = low +
+	                    ((target - arr[low]) * (high - low))
+	                    / (arr[high] - arr[low]);
+
+	            if (arr[pos] == target) {
+	                return pos;
+	            }
+
+	            if (arr[pos] < target) {
+	                low = pos + 1;
+	            } else {
+	                high = pos - 1;
+	            }
+	        }
+
+	        return -1;
+	    }
+	    public static void main(String[] args) {
+	    	int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+	    	int target = 70;
+	    	System.out.println(interpolationSearch(arr, target));
+	    }
 }
