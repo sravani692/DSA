dae898f32d9aa67466a2e1ac3ecec364a2d5046c
sravani <sirasapallisravani45@gmail.com>
2026-09-10 13:20:27 +0530

    update

--- src/Demo/Linkedlist.java
+++ src/Demo/Linkedlist.java
@@ -10,6 +10,61 @@ package Demo;
 
 public class Linkedlist {
 	
+    static class Node {
+
+	        int data;
+	        Node next;
+
+	        Node(int data) {
+	            this.data = data;
+	            this.next = null;
+	        }
+	    }
+
+	    Node head;
+
+	    void insertAtEnd(int data) {
+
+	        Node newNode = new Node(data);
+
+	        if (head == null) {
+	            head = newNode;
+	            return;
+	        }
+
+	        Node current = head;
+
+	        while (current.next != null) {
+	            current = current.next;
+	        }
+
+	        current.next = newNode;
+	    }
+
+	    void display() {
+
+	        Node current = head;
+
+	        while (current != null) {
+	            System.out.print(current.data + " -> ");
+	            current = current.next;
+	        }
+
+	        System.out.println("null");
+	    }
+
+	    public static void main(String[] args) {
+
+	        Linkedlist list = new Linkedlist();
+
+	        list.insertAtEnd(10);
+	        list.insertAtEnd(20);
+	        list.insertAtEnd(30);
+	        list.insertAtEnd(40);
+
+	        list.display();
+	    }
+	}
 		
-}
+
 
