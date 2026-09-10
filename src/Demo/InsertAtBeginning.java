316e7bdd92278ea515e480bf755d4e72a3f52649
sravani <sirasapallisravani45@gmail.com>
2026-09-10 11:12:51 +0530

    update

--- src/Demo/InsertAtBeginning.java
+++ src/Demo/InsertAtBeginning.java
@@ -2,5 +2,37 @@
 package Demo;
 
 public class InsertAtBeginning {
-	
+	static class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data = data;
+		}
+	}
+	Node head = null;
+	// Insert at beginning
+	void insertAtBeginning(int data) {
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+	}
+	//Display linked List
+	void display() {
+		Node current = head;
+		while(current != null) {
+			System.out.print(current.data + " -> ");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
+	public static void main(String[] args) {
+		InsertAtBeginning List = new InsertAtBeginning();
+		//Insert in reverse order
+		List.insertAtBeginning(30);
+		List.insertAtBeginning(20);
+		List.insertAtBeginning(10);
+		List.insertAtBeginning(5);
+	List.display();
+		
+		}
 }
